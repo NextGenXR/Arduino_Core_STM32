79ab9467e09dafd16a6757b7edc6cd1d51705740
Jenni O'Connor <jenni@nextGenxr.com>
2022-02-15 17:30:51 -0600

    Update includes, code clenup

--- cores/arduino/Stream.h
+++ cores/arduino/Stream.h
@@ -22,7 +22,7 @@
 #ifndef Stream_h
 #define Stream_h
 
-#include <inttypes.h>
+#include <cinttypes>
 #include "Print.h"
 
 // compatibility macros for testing
@@ -75,14 +75,14 @@ class Stream : public Print {
     bool find(const char *target);   // reads data from the stream until the target string is found
     bool find(const uint8_t *target)
     {
-      return find((const char *)target);
+      return find(reinterpret_cast<const char*>(target));
     }
     // returns true if target string is found, false if timed out (see setTimeout)
 
     bool find(const char *target, size_t length);   // reads data from the stream until the target string of given length is found
     bool find(const uint8_t *target, size_t length)
     {
-      return find((const char *)target, length);
+      return find(reinterpret_cast<const char*>(target), length);
     }
     // returns true if target string is found, false if timed out
 
@@ -94,13 +94,13 @@ class Stream : public Print {
     bool findUntil(const char *target, const char *terminator);   // as find but search ends if the terminator string is found
     bool findUntil(const uint8_t *target, const char *terminator)
     {
-      return findUntil((const char *)target, terminator);
+      return findUntil(reinterpret_cast<const char*>(target), terminator);
     }
 
     bool findUntil(const char *target, size_t targetLen, const char *terminate, size_t termLen);   // as above but search ends if the terminate string is found
     bool findUntil(const uint8_t *target, size_t targetLen, const char *terminate, size_t termLen)
     {
-      return findUntil((const char *)target, targetLen, terminate, termLen);
+      return findUntil(reinterpret_cast<const char*>(target), targetLen, terminate, termLen);
     }
 
     long parseInt(LookaheadMode lookahead = SKIP_ALL, char ignore = NO_IGNORE_CHAR);
@@ -116,7 +116,7 @@ class Stream : public Print {
     virtual size_t readBytes(char *buffer, size_t length);  // read chars from stream into buffer
     size_t readBytes(uint8_t *buffer, size_t length)
     {
-      return readBytes((char *)buffer, length);
+      return readBytes(reinterpret_cast<char*>(buffer), length);
     }
     // terminates if length characters have been read or timeout (see setTimeout)
     // returns the number of characters placed in the buffer (0 means no valid data found)
@@ -124,7 +124,7 @@ class Stream : public Print {
     virtual size_t readBytesUntil(char terminator, char *buffer, size_t length);  // as readBytes with terminator character
     size_t readBytesUntil(char terminator, uint8_t *buffer, size_t length)
     {
-      return readBytesUntil(terminator, (char *)buffer, length);
+      return readBytesUntil(terminator, reinterpret_cast<char*>(buffer), length);
     }
     // terminates if length characters have been read, timeout, or if the terminator character  detected
     // returns the number of characters placed in the buffer (0 means no valid data found)
