d307407ae9ad31a4ab807ebb897dbb4e25e7afc8
Jenni O'Connor <jenni@nextGenxr.com>
2022-03-20 13:42:31 -0500

    Compile errors on elif w/o defined

--- cores/arduino/stm32/LL/stm32yyxx_ll_rtc.h
+++ cores/arduino/stm32/LL/stm32yyxx_ll_rtc.h
@@ -5,39 +5,40 @@
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 #pragma GCC diagnostic ignored "-Wstrict-aliasing"
 
-#ifdef STM32F0xx
+#if defined (STM32F0xx)
   #include "stm32f0xx_ll_rtc.h"
-#elif STM32F1xx
+#elif defined  (STM32F1xx)
   #include "stm32f1xx_ll_rtc.h"
-#elif STM32F2xx
+#elif defined (STM32F2xx)
   #include "stm32f2xx_ll_rtc.h"
-#elif STM32F3xx
+#elif defined (STM32F3xx)
   #include "stm32f3xx_ll_rtc.h"
-#elif STM32F4xx
+#elif defined (STM32F4xx)
   #include "stm32f4xx_ll_rtc.h"
-#elif STM32F7xx
+  #include "stm32f4xx_hal_rtc.h"
+#elif defined (STM32F7xx)
   #include "stm32f7xx_ll_rtc.h"
-#elif STM32G0xx
+#elif defined (STM32G0xx)
   #include "stm32g0xx_ll_rtc.h"
-#elif STM32G4xx
+#elif defined  (STM32G4xx)
   #include "stm32g4xx_ll_rtc.h"
-#elif STM32H7xx
+#elif defined  (STM32H7xx)
   #include "stm32h7xx_ll_rtc.h"
-#elif STM32L0xx
+#elif defined  (STM32L0xx)
   #include "stm32l0xx_ll_rtc.h"
-#elif STM32L1xx
+#elif defined  (STM32L1xx)
   #include "stm32l1xx_ll_rtc.h"
-#elif STM32L4xx
+#elif defined  (STM32L4xx)
   #include "stm32l4xx_ll_rtc.h"
-#elif STM32L5xx
+#elif defined  (STM32L5xx)
   #include "stm32l5xx_ll_rtc.h"
-#elif STM32MP1xx
+#elif defined  (STM32MP1xx)
   #include "stm32mp1xx_ll_rtc.h"
-#elif STM32U5xx
+#elif defined  (STM32U5xx)
   #include "stm32u5xx_ll_rtc.h"
-#elif STM32WBxx
+#elif defined  (STM32WBxx)
   #include "stm32wbxx_ll_rtc.h"
-#elif STM32WLxx
+#elif defined  (STM32WLxx)
   #include "stm32wlxx_ll_rtc.h"
 #endif
 #pragma GCC diagnostic pop
