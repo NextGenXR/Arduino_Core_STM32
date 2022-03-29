9d3f6a47729bfbbbd3b0584076e982f01fcfafae
Jenni O'Connor <jenni@nextGenxr.com>
2022-03-29 18:56:49 -0500

    Correct the #ifelse

--- libraries/SrcWrapper/src/stm32/system_stm32yyxx.c
+++ libraries/SrcWrapper/src/stm32/system_stm32yyxx.c
@@ -1,37 +1,44 @@
-#define USE_HAL_DRIVER
 
-#ifdef STM32F0xx
+#ifndef USE_HAL_DRIVER
+#define USE_HAL_DRIVER
+#endif
+
+#if __has_include(<main.h>)
+ #include <main.h>
+ #endif
+
+#if defined(STM32F0xx)
   #include "system_stm32f0xx.c"
-#elif STM32F1xx
+#elif defined(STM32F1xx)
   #include "system_stm32f1xx.c"
-#elif STM32F2xx
+#elif defined(STM32F2xx)
   #include "system_stm32f2xx.c"
-#elif STM32F3xx
+#elif defined(STM32F3xx)
   #include "system_stm32f3xx.c"
-#elif STM32F4xx
+#elif defined(STM32F4xx)
   #include "system_stm32f4xx.c"
-#elif STM32F7xx
+#elif defined(STM32F7xx)
   #include "system_stm32f7xx.c"
-#elif STM32G0xx
+#elif defined(STM32G0xx)
   #include "system_stm32g0xx.c"
-#elif STM32G4xx
+#elif defined(STM32G4xx)
   #include "system_stm32g4xx.c"
-#elif STM32H7xx
+#elif defined(STM32H7xx)
   #include "system_stm32h7xx.c"
-#elif STM32L0xx
+#elif defined(STM32L0xx)
   #include "system_stm32l0xx.c"
-#elif STM32L1xx
+#elif defined(STM32L1xx)
   #include "system_stm32l1xx.c"
-#elif STM32L4xx
+#elif defined(STM32L4xx)
   #include "system_stm32l4xx.c"
-#elif STM32L5xx
+#elif defined(STM32L5xx)
   #include "system_stm32l5xx_ns.c"
-#elif STM32MP1xx
+#elif defined(STM32MP1xx)
   #include "system_stm32mp1xx.c"
-#elif STM32U5xx
+#elif defined(STM32U5xx)
   #include "system_stm32u5xx.c"
-#elif STM32WBxx
+#elif defined(STM32WBxx)
   #include "system_stm32wbxx.c"
-#elif STM32WLxx
+#elif defined(STM32WLxx)
   #include "system_stm32wlxx.c"
 #endif
