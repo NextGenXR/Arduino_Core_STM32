7a185f15f4bd37e354b95d88684a56660190c48b
Jenni O'Connor <jenni@nextgenxr.com>
2022-11-20 16:26:26 -0600

    Remove _Error_Handler

--- libraries/SoftwareSerial/src/SoftwareSerial.h
+++ libraries/SoftwareSerial/src/SoftwareSerial.h
@@ -86,7 +86,6 @@ class SoftwareSerial : public Stream {
     void setSpeed(uint32_t speed);
     void setRXTX(bool input);
     static void handleInterrupt();
-    void _Error_Handler(std::string message, int number);
 
   public:
     // public methods
