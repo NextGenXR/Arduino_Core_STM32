79540b8c131a520273ae083d27f0e9882b09a9fd
NextGenXR <jenni@nextgenxr.com>
2022-12-21 16:27:52 -0600

    Disable IRQ handlers for 1 and 3

--- libraries/SrcWrapper/src/stm32/interrupt.cpp
+++ libraries/SrcWrapper/src/stm32/interrupt.cpp
@@ -1,40 +1,40 @@
 /**
-  ******************************************************************************
-  * @file    interrupt.c
-  * @author  WI6LABS
-  * @version V1.0.0
-  * @date    01-August-2016
-  * @brief   provide an interface to enable/disable interruptions
-  *
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; COPYRIGHT(c) 2016 STMicroelectronics</center></h2>
-  *
-  * Redistribution and use in source and binary forms, with or without modification,
-  * are permitted provided that the following conditions are met:
-  *   1. Redistributions of source code must retain the above copyright notice,
-  *      this list of conditions and the following disclaimer.
-  *   2. Redistributions in binary form must reproduce the above copyright notice,
-  *      this list of conditions and the following disclaimer in the documentation
-  *      and/or other materials provided with the distribution.
-  *   3. Neither the name of STMicroelectronics nor the names of its contributors
-  *      may be used to endorse or promote products derived from this software
-  *      without specific prior written permission.
-  *
-  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file    interrupt.c
+ * @author  WI6LABS
+ * @version V1.0.0
+ * @date    01-August-2016
+ * @brief   provide an interface to enable/disable interruptions
+ *
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; COPYRIGHT(c) 2016 STMicroelectronics</center></h2>
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *   1. Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright notice,
+ *      this list of conditions and the following disclaimer in the documentation
+ *      and/or other materials provided with the distribution.
+ *   3. Neither the name of STMicroelectronics nor the names of its contributors
+ *      may be used to endorse or promote products derived from this software
+ *      without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ ******************************************************************************
+ */
 #include "interrupt.h"
 #include "lock_resource.h"
 #define LL_EXTI_MODULE_ENABLED
@@ -45,8 +45,8 @@
 
 /*As we can have only one interrupt/pin id, don't need to get the port info*/
 typedef struct {
-  IRQn_Type irqnb;
-  std::function<void(void)> callback;
+	IRQn_Type irqnb;
+	std::function<void(void)> callback;
 } gpio_irq_conf_str;
 
 /* Private_Defines */
@@ -89,53 +89,50 @@ static gpio_irq_conf_str gpio_irq_conf[NB_EXTI] = {
   {.irqnb = EXTI14_IRQn,    .callback = NULL}, //GPIO_PIN_14
   {.irqnb = EXTI15_IRQn,    .callback = NULL}  //GPIO_PIN_15
 #else
-  {.irqnb = EXTI0_IRQn,     .callback = NULL}, //GPIO_PIN_0
-  {.irqnb = EXTI1_IRQn,     .callback = NULL}, //GPIO_PIN_1
-  {.irqnb = EXTI2_IRQn,     .callback = NULL}, //GPIO_PIN_2
-  {.irqnb = EXTI3_IRQn,     .callback = NULL}, //GPIO_PIN_3
-  {.irqnb = EXTI4_IRQn,     .callback = NULL}, //GPIO_PIN_4
-  {.irqnb = EXTI9_5_IRQn,   .callback = NULL}, //GPIO_PIN_5
-  {.irqnb = EXTI9_5_IRQn,   .callback = NULL}, //GPIO_PIN_6
-  {.irqnb = EXTI9_5_IRQn,   .callback = NULL}, //GPIO_PIN_7
-  {.irqnb = EXTI9_5_IRQn,   .callback = NULL}, //GPIO_PIN_8
-  {.irqnb = EXTI9_5_IRQn,   .callback = NULL}, //GPIO_PIN_9
-  {.irqnb = EXTI15_10_IRQn, .callback = NULL}, //GPIO_PIN_10
-  {.irqnb = EXTI15_10_IRQn, .callback = NULL}, //GPIO_PIN_11
-  {.irqnb = EXTI15_10_IRQn, .callback = NULL}, //GPIO_PIN_12
-  {.irqnb = EXTI15_10_IRQn, .callback = NULL}, //GPIO_PIN_13
-  {.irqnb = EXTI15_10_IRQn, .callback = NULL}, //GPIO_PIN_14
-  {.irqnb = EXTI15_10_IRQn, .callback = NULL}  //GPIO_PIN_15
+		{ .irqnb = EXTI0_IRQn, .callback = NULL }, //GPIO_PIN_0
+		{ .irqnb = EXTI1_IRQn, .callback = NULL }, //GPIO_PIN_1
+		{ .irqnb = EXTI2_IRQn, .callback = NULL }, //GPIO_PIN_2
+		{ .irqnb = EXTI3_IRQn, .callback = NULL }, //GPIO_PIN_3
+		{ .irqnb = EXTI4_IRQn, .callback = NULL }, //GPIO_PIN_4
+		{ .irqnb = EXTI9_5_IRQn, .callback = NULL }, //GPIO_PIN_5
+		{ .irqnb = EXTI9_5_IRQn, .callback = NULL }, //GPIO_PIN_6
+		{ .irqnb = EXTI9_5_IRQn, .callback = NULL }, //GPIO_PIN_7
+		{ .irqnb = EXTI9_5_IRQn, .callback = NULL }, //GPIO_PIN_8
+		{ .irqnb = EXTI9_5_IRQn, .callback = NULL }, //GPIO_PIN_9
+		{ .irqnb = EXTI15_10_IRQn, .callback = NULL }, //GPIO_PIN_10
+		{ .irqnb = EXTI15_10_IRQn, .callback = NULL }, //GPIO_PIN_11
+		{ .irqnb = EXTI15_10_IRQn, .callback = NULL }, //GPIO_PIN_12
+		{ .irqnb = EXTI15_10_IRQn, .callback = NULL }, //GPIO_PIN_13
+		{ .irqnb = EXTI15_10_IRQn, .callback = NULL }, //GPIO_PIN_14
+		{ .irqnb = EXTI15_10_IRQn, .callback = NULL }  //GPIO_PIN_15
 #endif
 };
 
-
 static const uint32_t ll_exti_lines[NB_EXTI] = {
-  LL_EXTI_LINE_0,  LL_EXTI_LINE_1,  LL_EXTI_LINE_2,  LL_EXTI_LINE_3,
-  LL_EXTI_LINE_4,  LL_EXTI_LINE_5,  LL_EXTI_LINE_6,  LL_EXTI_LINE_7,
-  LL_EXTI_LINE_8,  LL_EXTI_LINE_9,  LL_EXTI_LINE_10, LL_EXTI_LINE_11,
-  LL_EXTI_LINE_12, LL_EXTI_LINE_13, LL_EXTI_LINE_14, LL_EXTI_LINE_15
-};
+LL_EXTI_LINE_0, LL_EXTI_LINE_1, LL_EXTI_LINE_2, LL_EXTI_LINE_3,
+LL_EXTI_LINE_4, LL_EXTI_LINE_5, LL_EXTI_LINE_6, LL_EXTI_LINE_7,
+LL_EXTI_LINE_8, LL_EXTI_LINE_9, LL_EXTI_LINE_10, LL_EXTI_LINE_11,
+LL_EXTI_LINE_12, LL_EXTI_LINE_13, LL_EXTI_LINE_14, LL_EXTI_LINE_15 };
 /* Private Functions */
 /**
-  * @brief  This function returns the pin ID function of the HAL PIN definition
-  * @param  pin : one of the gpio pin
-  * @retval None
-  */
-static uint8_t get_pin_id(uint16_t pin)
-{
-  uint8_t id = 0;
+ * @brief  This function returns the pin ID function of the HAL PIN definition
+ * @param  pin : one of the gpio pin
+ * @retval None
+ */
+static uint8_t get_pin_id(uint16_t pin) {
+	uint8_t id = 0;
 
-  while (pin != 0x0001) {
-    pin = pin >> 1;
-    id++;
-  }
+	while (pin != 0x0001) {
+		pin = pin >> 1;
+		id++;
+	}
 
-  return id;
+	return id;
 }
-void stm32_interrupt_enable(GPIO_TypeDef *port, uint16_t pin, callback_function_t callback, uint32_t mode)
-{
-  GPIO_InitTypeDef GPIO_InitStruct;
-  uint8_t id = get_pin_id(pin);
+void stm32_interrupt_enable(GPIO_TypeDef *port, uint16_t pin,
+		callback_function_t callback, uint32_t mode) {
+	GPIO_InitTypeDef GPIO_InitStruct;
+	uint8_t id = get_pin_id(pin);
 
 #ifdef STM32F1xx
   uint8_t position;
@@ -144,24 +141,24 @@ void stm32_interrupt_enable(GPIO_TypeDef *port, uint16_t pin, callback_function_
   volatile uint32_t *CRxRegister;
   const uint32_t ConfigMask = 0x00000008; //MODE0 == 0x0 && CNF0 == 0x2
 #else
-  uint32_t pull;
+	uint32_t pull;
 #endif /* STM32F1xx */
 
-  // GPIO pin configuration
-  GPIO_InitStruct.Pin       = pin;
-  GPIO_InitStruct.Mode      = mode;
+	// GPIO pin configuration
+	GPIO_InitStruct.Pin = pin;
+	GPIO_InitStruct.Mode = mode;
 
-  //read the pull mode directly in the register as no function exists to get it.
-  //Do it in case the user already defines the IO through the digital io
-  //interface
+	//read the pull mode directly in the register as no function exists to get it.
+	//Do it in case the user already defines the IO through the digital io
+	//interface
 #ifndef STM32F1xx
-  pull = port->PUPDR;
+	pull = port->PUPDR;
 #ifdef GPIO_PUPDR_PUPD0
   pull &= (GPIO_PUPDR_PUPD0 << (id * 2));
   GPIO_InitStruct.Pull = (GPIO_PUPDR_PUPD0 & (pull >> (id * 2)));
 #else
-  pull &= (GPIO_PUPDR_PUPDR0 << (id * 2));
-  GPIO_InitStruct.Pull = (GPIO_PUPDR_PUPDR0 & (pull >> (id * 2)));
+	pull &= (GPIO_PUPDR_PUPDR0 << (id * 2));
+	GPIO_InitStruct.Pull = (GPIO_PUPDR_PUPDR0 & (pull >> (id * 2)));
 #endif /* GPIO_PUPDR_PUPD0 */
 #else
   CRxRegister = (pin < GPIO_PIN_8) ? &port->CRL : &port->CRH;
@@ -184,71 +181,70 @@ void stm32_interrupt_enable(GPIO_TypeDef *port, uint16_t pin, callback_function_
   }
 #endif /* STM32F1xx */
 
-  GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_HIGH;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
 
-  hsem_lock(CFG_HW_GPIO_SEMID, HSEM_LOCK_DEFAULT_RETRY);
+	hsem_lock(CFG_HW_GPIO_SEMID, HSEM_LOCK_DEFAULT_RETRY);
 
-  HAL_GPIO_Init(port, &GPIO_InitStruct);
+	HAL_GPIO_Init(port, &GPIO_InitStruct);
 
-  hsem_unlock(CFG_HW_GPIO_SEMID);
+	hsem_unlock(CFG_HW_GPIO_SEMID);
 
-  gpio_irq_conf[id].callback = callback;
+	gpio_irq_conf[id].callback = callback;
 
-  // Enable and set EXTI Interrupt
-  HAL_NVIC_SetPriority(gpio_irq_conf[id].irqnb, EXTI_IRQ_PRIO, EXTI_IRQ_SUBPRIO);
-  HAL_NVIC_EnableIRQ(gpio_irq_conf[id].irqnb);
+	// Enable and set EXTI Interrupt
+	HAL_NVIC_SetPriority(gpio_irq_conf[id].irqnb, EXTI_IRQ_PRIO,
+	EXTI_IRQ_SUBPRIO);
+	HAL_NVIC_EnableIRQ(gpio_irq_conf[id].irqnb);
 }
 
 /**
-  * @brief  This function enable the interruption on the selected port/pin
-  * @param  port : one of the gpio port
-  * @param  pin : one of the gpio pin
-  **@param  callback : callback to call when the interrupt falls
-  * @param  mode : one of the supported interrupt mode defined in stm32_hal_gpio
-  * @retval None
-  */
-void stm32_interrupt_enable(GPIO_TypeDef *port, uint16_t pin, void (*callback)(void), uint32_t mode)
-{
-  std::function<void(void)> _c = callback;
-  stm32_interrupt_enable(port, pin, _c, mode);
+ * @brief  This function enable the interruption on the selected port/pin
+ * @param  port : one of the gpio port
+ * @param  pin : one of the gpio pin
+ **@param  callback : callback to call when the interrupt falls
+ * @param  mode : one of the supported interrupt mode defined in stm32_hal_gpio
+ * @retval None
+ */
+void stm32_interrupt_enable(GPIO_TypeDef *port, uint16_t pin,
+		void (*callback)(void), uint32_t mode) {
+	std::function<void(void)> _c = callback;
+	stm32_interrupt_enable(port, pin, _c, mode);
 
 }
 
 /**
-  * @brief  This function disable the interruption on the selected port/pin
-  * @param  port : one of the gpio port
-  * @param  pin : one of the gpio pin
-  * @retval None
-  */
-void stm32_interrupt_disable(GPIO_TypeDef *port, uint16_t pin)
-{
-  UNUSED(port);
-  uint8_t id = get_pin_id(pin);
-  gpio_irq_conf[id].callback = NULL;
+ * @brief  This function disable the interruption on the selected port/pin
+ * @param  port : one of the gpio port
+ * @param  pin : one of the gpio pin
+ * @retval None
+ */
+void stm32_interrupt_disable(GPIO_TypeDef *port, uint16_t pin) {
+	UNUSED(port);
+	uint8_t id = get_pin_id(pin);
+	gpio_irq_conf[id].callback = NULL;
 
-  for (int i = 0; i < NB_EXTI; i++) {
-    if (gpio_irq_conf[id].irqnb == gpio_irq_conf[i].irqnb
-        && gpio_irq_conf[i].callback != NULL) {
-      return;
-    }
-  }
+	for (int i = 0; i < NB_EXTI; i++) {
+		if (gpio_irq_conf[id].irqnb
+				== gpio_irq_conf[i].irqnb&& gpio_irq_conf[i].callback != NULL) {
+			return;
+		}
+	}
 
-  LL_EXTI_DisableIT_0_31(ll_exti_lines[id]);
-  HAL_NVIC_DisableIRQ(gpio_irq_conf[id].irqnb);
+	LL_EXTI_DisableIT_0_31(ll_exti_lines[id]);
+	HAL_NVIC_DisableIRQ(gpio_irq_conf[id].irqnb);
 }
 
 /**
-  * @brief This function his called by the HAL if the IRQ is valid
-  * @param  GPIO_Pin : one of the gpio pin
-  * @retval None
-  */
-void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
-{
-  uint8_t irq_id = get_pin_id(GPIO_Pin);
+ * @brief This function his called by the HAL if the IRQ is valid
+ * @param  GPIO_Pin : one of the gpio pin
+ * @retval None
+ */
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
+	uint8_t irq_id = get_pin_id(GPIO_Pin);
 
-  if (gpio_irq_conf[irq_id].callback != NULL) {
-    gpio_irq_conf[irq_id].callback();
-  }
+	if (gpio_irq_conf[irq_id].callback != NULL) {
+		gpio_irq_conf[irq_id].callback();
+	}
 }
 
 #if defined(STM32G0xx) || defined(STM32MP1xx) || defined(STM32L5xx) || defined(STM32U5xx)
@@ -325,80 +321,78 @@ void EXTI4_15_IRQHandler(void)
 extern "C" {
 #endif
 /**
-  * @brief This function handles external line 0 interrupt request.
-  * @param  None
-  * @retval None
-  */
-void EXTI0_IRQHandler(void)
+ * @brief This function handles external line 0 interrupt request.
+ * @param  None
+ * @retval None
+ */
+#ifdef USE_IRQ0
+WEAK void EXTI0_IRQHandler(void)
 {
-  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_0);
+	HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_0);
+}
+#endif
+
+/**
+ * @brief This function handles external line 1 interrupt request.
+ * @param  None
+ * @retval None
+ */
+WEAK void EXTI1_IRQHandler(void) {
+	HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_1);
 }
 
 /**
-  * @brief This function handles external line 1 interrupt request.
-  * @param  None
-  * @retval None
-  */
-void EXTI1_IRQHandler(void)
-{
-  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_1);
+ * @brief This function handles external line 2 interrupt request.
+ * @param  None
+ * @retval None
+ */
+WEAK void _EXTI2_IRQHandler(void) {
+	HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_2);
 }
 
 /**
-  * @brief This function handles external line 2 interrupt request.
-  * @param  None
-  * @retval None
-  */
-void _EXTI2_IRQHandler(void)
-{
-  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_2);
-}
-
-/**
-  * @brief This function handles external line 3 interrupt request.
-  * @param  None
-  * @retval None
-  */
-void EXTI3_IRQHandler(void)
+ * @brief This function handles external line 3 interrupt request.
+ * @param  None
+ * @retval None
+ */
+#ifdef USE_IRQ3
+WEAK void EXTI3_IRQHandler(void)
 {
   HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_3);
 }
-
+#endif
 /**
-  * @brief This function handles external line 4 interrupt request.
-  * @param  None
-  * @retval None
-  */
-void EXTI4_IRQHandler(void)
-{
-  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_4);
+ * @brief This function handles external line 4 interrupt request.
+ * @param  None
+ * @retval None
+ */
+void EXTI4_IRQHandler(void) {
+	HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_4);
 }
 
 #if !defined(STM32MP1xx) && !defined(STM32L5xx) && !defined(STM32U5xx)
 /**
-  * @brief This function handles external line 5 to 9 interrupt request.
-  * @param  None
-  * @retval None
-  */
-void EXTI9_5_IRQHandler(void)
-{
-  uint32_t pin;
-  for (pin = GPIO_PIN_5; pin <= GPIO_PIN_9; pin = pin << 1) {
-    HAL_GPIO_EXTI_IRQHandler(pin);
-  }
+ * @brief This function handles external line 5 to 9 interrupt request.
+ * @param  None
+ * @retval None
+ */
+void EXTI9_5_IRQHandler(void) {
+	uint32_t pin;
+	for (pin = GPIO_PIN_5; pin <= GPIO_PIN_9; pin = pin << 1) {
+		HAL_GPIO_EXTI_IRQHandler(pin);
+	}
 }
 
 /**
-  * @brief This function handles external line 10 to 15 interrupt request.
-  * @param  None
-  * @retval None
-  */
-void EXTI15_10_IRQHandler(void)
-{
-  uint32_t pin;
-  for (pin = GPIO_PIN_10; pin <= GPIO_PIN_15; pin = pin << 1) {
-    HAL_GPIO_EXTI_IRQHandler(pin);
-  }
+ * @brief This function handles external line 10 to 15 interrupt request.
+ * @param  None
+ * @retval None
+ */
+void EXTI15_10_IRQHandler(void) {
+	uint32_t pin;
+	for (pin = GPIO_PIN_10; pin <= GPIO_PIN_15; pin = pin << 1) {
+		HAL_GPIO_EXTI_IRQHandler(pin);
+	}
 }
 #else /* STM32MP1xx && STM32L5xx && STM32U5xx */
 
